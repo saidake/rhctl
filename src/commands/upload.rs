32f9271c2329f85f5734d70f0257aea33837deea
Craig Brown <craigbrowndev@gmail.com>
2025-10-16 04:36:58 +0800

    [Global] chore: Add Apache 2.0 License

--- src/commands/upload.rs
+++ src/commands/upload.rs
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * **************************************************************************
+ * Upload a file or directory contents into a remote server directory.
+ * 
+ * Author: Craig Brown
+ * Date: October 16, 2025
+ * Since: 1.0.0
+ */
+
 use futures::future::join_all;
 use std::collections::HashMap;
 use std::path::Path;
--- src/common/ssh_pool.rs
+++ src/common/ssh_pool.rs
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * **************************************************************************
+ * An SSH connection pool for managing sessions and channels across 
+ * multiple servers.
+ * 
+ * Author: Craig Brown
+ * Date: October 16, 2025
+ * Since: 1.0.0
+ */
 use crate::domain::cmd_params::ServerMetadata;
 use crate::domain::constants::{
     DEFAULT_SSH_HANDSHAKE_TIMEOUT, DEFAULT_SSH_PORT, REMOTE_TEMP_SBXCTL_FOLDER, SYSTEM_TASK_NAME,
